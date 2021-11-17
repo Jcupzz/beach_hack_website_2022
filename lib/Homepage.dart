9c23114654ac93b280ba36de8e79f0a4c313d5a6
Jcupzz <jcupzz255@gmail.com>
2021-11-17 19:47:05 +0530

    commented out for mobile testing

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,30 +1,37 @@
+import 'dart:html';
+
 import 'package:beach_hack_website_2022/Player.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/particles.dart';
 import 'package:flame/sprite.dart';
+import 'package:flame/widgets.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:ui';
 
-enum PlayerState {
-  idleLeft,
-  idleRight,
-  swimmingLeft,
-  swimmingRight,
-}
+// enum PlayerState {
+//   idleLeft,
+//   idleRight,
+//   swimmingLeft,
+//   swimmingRight,
+// }
 
 class Homepage extends FlameGame {
   late TiledComponent tiledMap;
-  late SpriteAnimationGroupComponent<PlayerState> playerGroup;
+  // late SpriteAnimationGroupComponent<PlayerState> playerGroup;
 
   @override
   void onGameResize(Vector2 canvasSize) {
     // TODO: implement onGameResize
-    super.onGameResize(canvasSize);
 
-    camera.setRelativeOffset(Anchor.topLeft);
+    super.onGameResize(canvasSize);
+    print("ttsts");
+
+    // camera.setRelativeOffset(Anchor.topLeft);
     // camera.viewport = FixedResolutionViewport(Vector2(size.x, 1072));
-    camera.gameSize.y = 1072;
+    // camera.gameSize.y = 1072;
   }
 
   @override
@@ -37,34 +44,45 @@ class Homepage extends FlameGame {
   Future<void>? onLoad() async {
     // TODO: implement onLoad
     print(size.toString());
+    print(WidgetsBinding.instance?.window.physicalSize.toString());
 
     final tiledMap = await TiledComponent.load('beach.tmx', Vector2.all(16));
 
     add(tiledMap);
 
-    final IdleGuyImage = await Flame.images.load('idle_guy_1.png');
+    // final IdleGuyImage = await Flame.images.load('idle_guy_1.png');
 
-    final IdleGuySpriteSheet = SpriteSheet(
-      image: IdleGuyImage,
-      srcSize: Vector2(IdleGuyImage.width / 5, IdleGuyImage.height.toDouble()),
-    );
+    // final IdleGuySpriteSheet = SpriteSheet(
+    //   image: IdleGuyImage,
+    //   srcSize: Vector2(IdleGuyImage.width / 5, IdleGuyImage.height.toDouble()),
+    // );
 
-    final IdleRightSpriteAnimation = IdleGuySpriteSheet.createAnimation(
-        row: 0, stepTime: 0.15, from: 0, to: 6);
+    // final IdleRightSpriteAnimation = IdleGuySpriteSheet.createAnimation(
+    //     row: 0, stepTime: 0.15, from: 0, to: 6);
 
-    playerGroup = SpriteAnimationGroupComponent(
-      animations: {
-        PlayerState.idleRight: IdleRightSpriteAnimation,
-      },
-      current: PlayerState.idleRight,
-      position: Vector2(200, 200),
-      size: Vector2(84.4, 158),
-    );
+    // playerGroup = SpriteAnimationGroupComponent(
+    //   animations: {
+    //     PlayerState.idleRight: IdleRightSpriteAnimation,
+    //   },
+    //   current: PlayerState.idleRight,
+    //   position: Vector2(size.x / 2, size.y / 2),
+    //   size: Vector2(84.4, 158),
+    // );
 
-    add(playerGroup);
+    // // add(playerGroup);
+    // print("Size of world is: " + size.x.toString());
+    // camera.viewport = FixedResolutionViewport((Vector2(1920, 1072)));
 
-    camera.followComponent(playerGroup);
+    // camera.followComponent(playerGroup);
+
+    // camera.worldBounds = Rect.fromLTRB(0, 0, 1920, 1072);
 
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+    super.update(dt);
   }
 }
