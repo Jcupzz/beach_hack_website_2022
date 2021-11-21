b4fe7e5e4eabf6fd3c8095b8e94e5def1759c6a4
Jcupzz <jcupzz255@gmail.com>
2021-11-21 20:00:16 +0530

    added release version

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "4b245fa28cf68844a9567cf682913f68",
-"/": "4b245fa28cf68844a9567cf682913f68",
+"index.html": "03757ebd7800dd01bee1d3ae9b94e404",
+"/": "03757ebd7800dd01bee1d3ae9b94e404",
 "main.dart.js": "fbe7689bd0b1202864b411ae20337357",
 "manifest.json": "f3eead5b72274618e9115d2fdd9243b1",
 "version.json": "1a51d5cb96c431c4147c15da695378ee"
